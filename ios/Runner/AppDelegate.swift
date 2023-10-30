98b05e95b799072d0b4bf6778bbccfb4073bea7b
Iris2783 <iris.corp.2783@gmail.com>
2023-10-31 07:34:04 +0900

    Add a time interval for splash screen

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,14 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        // スプラッシュ画面の表示時間を設定（例: 3秒）
+        Thread.sleep(forTimeInterval: 3.0)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
+
