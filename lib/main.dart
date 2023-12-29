271968d190035753fb7dc5c5cc9b4ed4dca763c4
Iris2783 <iris.corp.2783@gmail.com>
2023-12-29 15:15:25 +0900

    constを追加

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: TopPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
