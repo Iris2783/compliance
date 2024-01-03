bcdcd0993bd94c73a312dc8bdd7ff8b37753dd01
Iris2783 <iris.corp.2783@gmail.com>
2024-01-03 18:20:32 +0900

    Responsive layout

---
+++ lib/Responsive/responsive_layout.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  final Widget mobileScaffold;
+  final Widget tabletScaffold;
+  final Widget desktopScaffold;
+
+  // ignore: use_key_in_widget_constructors
+  const ResponsiveLayout({
+    required this.mobileScaffold,
+    required this.tabletScaffold,
+    required this.desktopScaffold,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < 600) {
+          return mobileScaffold;
+        } else if (constraints.maxWidth < 1100) {
+          return tabletScaffold;
+        } else {
+          return desktopScaffold;
+        }
+      },
+    );
+  }
+}
