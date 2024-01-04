eb16c1892209c9a0d2b0f49dcbd8a15fd3177312
Iris2783 <iris.corp.2783@gmail.com>
2024-01-05 00:13:52 +0900

    Add Row widget

--- lib/Details/LaborManagement/desktop_labormanagement.dart
+++ lib/Details/LaborManagement/desktop_labormanagement.dart
@@ -23,66 +23,138 @@ class DesktopLaborManagement extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Row(
             children: [
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: const Color.fromRGBO(133, 174, 77, 0.3),
-                ),
-                padding: const EdgeInsets.all(8.0),
-                child: const Text(
-                  '労働基準法について',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '労働基準法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '労働基準法は、労働基準監督署による監督指導や罰則によって、その履行が確保されます。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '労働契約法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '労働契約法に定められた事項を含む民事上の紛争については、簡易・迅速に解決するための仕組として、「個別労働紛争解決システム」が用意されています。これは、労働問題への高い専門性を有する都道府県労働局において、無料で個別労働紛争の解決援助サービスを提供するもので、',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '・総合労働相談コーナーにおける情報提供・相談\n'
+                      '・都道府県労働局長による助言・指導\n'
+                      '・紛争調整委員会によるあっせん\n'
+                      'があります。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '男女雇用機会均等法、育児・介護休業法、パートタイム・有期雇用労働法、労働施策総合推進法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'これらの法律については、各都道府県労働局雇用環境・均等部（室）による助言・指導・勧告によって、その履行が確保されます。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
               ),
-              const SizedBox(height: 8),
-              const Text(
-                '労働基準法は、労働基準監督署による監督指導や罰則によって、その履行が確保されます。',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: const Color.fromRGBO(133, 174, 77, 0.3),
+              const SizedBox(width: 16), // 左右のContainerの間に隙間を追加
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '労働基準法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '労働基準法は、労働基準監督署による監督指導や罰則によって、その履行が確保されます。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '労働契約法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '労働契約法に定められた事項を含む民事上の紛争については、簡易・迅速に解決するための仕組として、「個別労働紛争解決システム」が用意されています。これは、労働問題への高い専門性を有する都道府県労働局において、無料で個別労働紛争の解決援助サービスを提供するもので、',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      '・総合労働相談コーナーにおける情報提供・相談\n'
+                      '・都道府県労働局長による助言・指導\n'
+                      '・紛争調整委員会によるあっせん\n'
+                      'があります。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                    const SizedBox(height: 16),
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        color: const Color.fromRGBO(133, 174, 77, 0.3),
+                      ),
+                      padding: const EdgeInsets.all(8.0),
+                      child: const Text(
+                        '男女雇用機会均等法、育児・介護休業法、パートタイム・有期雇用労働法、労働施策総合推進法について',
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    const Text(
+                      'これらの法律については、各都道府県労働局雇用環境・均等部（室）による助言・指導・勧告によって、その履行が確保されます。',
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ],
                 ),
-                padding: const EdgeInsets.all(8.0),
-                child: const Text(
-                  '労働契約法について',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                '労働契約法に定められた事項を含む民事上の紛争については、簡易・迅速に解決するための仕組として、「個別労働紛争解決システム」が用意されています。これは、労働問題への高い専門性を有する都道府県労働局において、無料で個別労働紛争の解決援助サービスを提供するもので、',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                '・総合労働相談コーナーにおける情報提供・相談\n'
-                '・都道府県労働局長による助言・指導\n'
-                '・紛争調整委員会によるあっせん\n'
-                'があります。',
-                style: TextStyle(fontSize: 16),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  color: const Color.fromRGBO(133, 174, 77, 0.3),
-                ),
-                padding: const EdgeInsets.all(8.0),
-                child: const Text(
-                  '男女雇用機会均等法、育児・介護休業法、パートタイム・有期雇用労働法、労働施策総合推進法について',
-                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                ),
-              ),
-              const SizedBox(height: 8),
-              const Text(
-                'これらの法律については、各都道府県労働局雇用環境・均等部（室）による助言・指導・勧告によって、その履行が確保されます。',
-                style: TextStyle(fontSize: 16),
               ),
             ],
           ),
