3a253271115f5766cf89735347866406f14ba047
Iris2783 <iris.corp.2783@gmail.com>
2024-01-05 15:44:01 +0900

    Modify the table

--- lib/Parts/LaborManagement/desktop_table_left1.dart
+++ lib/Parts/LaborManagement/desktop_table_left1.dart
@@ -2,17 +2,16 @@
 import 'package:flutter/material.dart';
 
 class DesktopTableLeft1 extends StatelessWidget {
+  final double cellWidth = 100.0;
+  final Map<int, double> rowHeights = {
+    0: 30.0,
+    1: 30.0,
+    2: 30.0,
+    3: 30.0,
+  };
+
   @override
   Widget build(BuildContext context) {
-    // 各行の高さを指定するマップ
-    final Map<int, double> rowHeights = {
-      0: 30.0, // ヘッダー行の高さ
-      1: 30.0,
-      2: 30.0,
-      3: 30.0,
-      // 追加の行の高さを必要に応じて指定 増やしたい場合も簡易的に実装が可能
-    };
-
     return Table(
       columnWidths: const {
         0: FixedColumnWidth(100.0),
@@ -21,43 +20,53 @@ class DesktopTableLeft1 extends StatelessWidget {
       },
       border: TableBorder.all(),
       children: [
-        buildTableRow(
-          [
-            'Header 1',
-            'Header 2',
-            'Header 3',
-          ],
-          isHeader: true,
-          height: rowHeights[0], // ヘッダー行の高さ
-        ),
+        buildTableRow(['Header 1', 'Header 2', 'Header 3'], isHeader: true, height: rowHeights[0]),
         buildTableRow(['Data 1', 'Data 2', 'Data 3'], height: rowHeights[1]),
         buildTableRow(['Data 5', 'Data 6', 'Data 7'], height: rowHeights[2]),
         buildTableRow(['Data 9', 'Data 10', 'Data 11'], height: rowHeights[3]),
-        // 追加の行を必要に応じて追加し、各行の高さを指定
       ],
     );
   }
 
   TableRow buildTableRow(List<String> rowData, {bool isHeader = false, double? height}) {
     return TableRow(
-      decoration: BoxDecoration(
-        color: isHeader ? Colors.blue : null,
-      ),
-      children: rowData.map(
-        (cellData) {
+      children: rowData.asMap().entries.map(
+        (entry) {
+          final int columnIndex = entry.key;
+          final String cellData = entry.value;
           return TableCell(
-            verticalAlignment: isHeader ? TableCellVerticalAlignment.middle : TableCellVerticalAlignment.top,
             child: SizedBox(
-              height: height, // 各行の高さを指定
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: isHeader
-                    ? Text(
-                        cellData,
-                        style: const TextStyle(color: Colors.white),
-                      )
-                    : Text(cellData),
-              ),
+              height: height,
+              child: isHeader
+                  ? Container(
+                      color: const Color.fromRGBO(133, 177, 77, 1),
+                      width: cellWidth,
+                      child: Center(
+                        child: Text(
+                          cellData,
+                          style: const TextStyle(
+                            color: Colors.white,
+                            // fontWeight: FontWeight.bold,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container(
+                      color: (columnIndex == 0 && !isHeader) ? const Color.fromRGBO(133, 177, 77, 1) : null,
+                      width: cellWidth,
+                      height: height,
+                      child: Center(
+                        child: Text(
+                          cellData,
+                          style: TextStyle(
+                            color: (columnIndex == 0 && !isHeader) ? Colors.white : Colors.black,
+                            fontWeight: FontWeight.normal,
+                            fontSize: 14.0,
+                          ),
+                        ),
+                      ),
+                    ),
             ),
           );
         },
