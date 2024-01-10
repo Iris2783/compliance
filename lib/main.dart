593711156dd5e0383ee139a776cd371634f332b8
Iris2783 <iris.corp.2783@gmail.com>
2024-01-10 13:32:12 +0900

    some update

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
+  GoogleFonts.config.allowRuntimeFetching = false;
   runApp(const MyApp());
 }
 
