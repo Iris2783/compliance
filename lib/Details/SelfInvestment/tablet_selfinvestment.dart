f76f96b4b334f3a880bcdd2a362083e5ee366e6a
Iris2783 <iris.corp.2783@gmail.com>
2024-01-12 16:37:09 +0900

    New file

---
+++ lib/Details/SelfInvestment/tablet_selfinvestment.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: file_names, use_key_in_widget_constructors
+import 'package:flutter/material.dart';
+
+class TabletSelfInvestment extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back, color: Colors.white),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        title: const Center(
+          child: Text(
+            '社員投資',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+        backgroundColor: const Color.fromRGBO(133, 174, 77, 1),
+      ),
+    );
+  }
+}
