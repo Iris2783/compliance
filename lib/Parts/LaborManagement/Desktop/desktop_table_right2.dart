99c078b86479633d11e1e16ba5ab7837a1b644d7
Iris2783 <108006833+Iris2783@users.noreply.github.com>
2024-01-23 08:40:05 +0900

    Update desktop_table_right2.dart

--- lib/Parts/LaborManagement/Desktop/desktop_table_right2.dart
+++ lib/Parts/LaborManagement/Desktop/desktop_table_right2.dart
@@ -16,22 +16,28 @@ class DesktopTableRight2LM extends StatelessWidget {
     return Table(
       columnWidths: const {
         0: FixedColumnWidth(80.0),
-        1: FixedColumnWidth(230.0),
-        2: FixedColumnWidth(230.0),
-        3: FixedColumnWidth(230.0),
+        1: FixedColumnWidth(255.0),
+        2: FixedColumnWidth(255.0),
+        3: FixedColumnWidth(255.0),
       }, //列の幅を指定
       border: TableBorder.all(),
       children: [
-        buildTableRow(['月日数', '基準労働時間', '上限労働時間', '該当月'], isHeader: true, height: rowHeights[0]),
-        buildTableRow(['31日', '177時間08分', '257時間08分', '1、3、5、7、8、10、12'], height: rowHeights[1]),
-        buildTableRow(['30日', '171時間25分', '251時間25分', '4、6、9、11'], height: rowHeights[2]),
-        buildTableRow(['29日', '165時間42分', '245時間42分', '2(うるう年)'], height: rowHeights[3]),
-        buildTableRow(['28日', '160時間00分', '240時間00分', '2'], height: rowHeights[4]),
+        buildTableRow(['月日数', '基準労働時間', '上限労働時間', '該当月'],
+            isHeader: true, height: rowHeights[0]),
+        buildTableRow(['31日', '177時間08分', '257時間08分', '1、3、5、7、8、10、12'],
+            height: rowHeights[1]),
+        buildTableRow(['30日', '171時間25分', '251時間25分', '4、6、9、11'],
+            height: rowHeights[2]),
+        buildTableRow(['29日', '165時間42分', '245時間42分', '2(うるう年)'],
+            height: rowHeights[3]),
+        buildTableRow(['28日', '160時間00分', '240時間00分', '2'],
+            height: rowHeights[4]),
       ],
     );
   }
 
-  TableRow buildTableRow(List<String> rowData, {bool isHeader = false, double? height}) {
+  TableRow buildTableRow(List<String> rowData,
+      {bool isHeader = false, double? height}) {
     return TableRow(
       children: rowData.asMap().entries.map(
         (entry) {
@@ -50,22 +56,26 @@ class DesktopTableRight2LM extends StatelessWidget {
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
+                            fontSize: 20.0,
                           ),
                         ),
                       ),
                     )
                   : Container(
-                      color: (columnIndex == 0 && !isHeader) ? const Color.fromRGBO(133, 177, 77, 1) : null,
+                      color: (columnIndex == 0 && !isHeader)
+                          ? const Color.fromRGBO(133, 177, 77, 1)
+                          : null,
                       width: cellWidth,
                       height: height,
                       child: Center(
                         child: Text(
                           cellData,
                           style: TextStyle(
-                            color: (columnIndex == 0 && !isHeader) ? Colors.white : Colors.black,
+                            color: (columnIndex == 0 && !isHeader)
+                                ? Colors.white
+                                : Colors.black,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
+                            fontSize: 20.0,
                           ),
                         ),
                       ),
