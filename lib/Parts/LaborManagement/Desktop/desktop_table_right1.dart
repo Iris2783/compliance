29f2c93341414593657ea7a8b0518a0073396afc
Iris2783 <108006833+Iris2783@users.noreply.github.com>
2024-01-23 08:39:49 +0900

    Update desktop_table_right1.dart

--- lib/Parts/LaborManagement/Desktop/desktop_table_right1.dart
+++ lib/Parts/LaborManagement/Desktop/desktop_table_right1.dart
@@ -13,18 +13,21 @@ class DesktopTableRight1LM extends StatelessWidget {
     return Table(
       columnWidths: const {
         0: FixedColumnWidth(80.0),
-        1: FixedColumnWidth(280.0),
-        2: FixedColumnWidth(280.0),
+        1: FixedColumnWidth(380.0),
+        2: FixedColumnWidth(380.0),
       }, //列の幅を指定
       border: TableBorder.all(),
       children: [
-        buildTableRow([' ', '原則', '特別延長申請時'], isHeader: true, height: rowHeights[0]),
-        buildTableRow(['月間', '80時間(ソフトリミット60時間)', '不可(80時間超過禁止)'], height: rowHeights[1]),
+        buildTableRow([' ', '原則', '特別延長申請時'],
+            isHeader: true, height: rowHeights[0]),
+        buildTableRow(['月間', '80時間(ソフトリミット60時間)', '不可(80時間超過禁止)'],
+            height: rowHeights[1]),
       ],
     );
   }
 
-  TableRow buildTableRow(List<String> rowData, {bool isHeader = false, double? height}) {
+  TableRow buildTableRow(List<String> rowData,
+      {bool isHeader = false, double? height}) {
     return TableRow(
       children: rowData.asMap().entries.map(
         (entry) {
@@ -43,22 +46,26 @@ class DesktopTableRight1LM extends StatelessWidget {
                           style: const TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
+                            fontSize: 20.0,
                           ),
                         ),
                       ),
                     )
                   : Container(
-                      color: (columnIndex == 0 && !isHeader) ? const Color.fromRGBO(133, 177, 77, 1) : null,
+                      color: (columnIndex == 0 && !isHeader)
+                          ? const Color.fromRGBO(133, 177, 77, 1)
+                          : null,
                       width: cellWidth,
                       height: height,
                       child: Center(
                         child: Text(
                           cellData,
                           style: TextStyle(
-                            color: (columnIndex == 0 && !isHeader) ? Colors.white : Colors.black,
+                            color: (columnIndex == 0 && !isHeader)
+                                ? Colors.white
+                                : Colors.black,
                             fontWeight: FontWeight.bold,
-                            fontSize: 18.0,
+                            fontSize: 20.0,
                           ),
                         ),
                       ),
