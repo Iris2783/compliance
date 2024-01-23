97271b655a4878fa09449fe94cbfdbf0b20ef2b2
Iris2783 <108006833+Iris2783@users.noreply.github.com>
2024-01-23 14:56:17 +0900

    Update textparts3.dart

--- lib/Parts/TextParts/textparts3.dart
+++ lib/Parts/TextParts/textparts3.dart
@@ -4,19 +4,27 @@ class TextParts3 extends StatelessWidget {
   final String bulletPoint3;
   final List<TextPart3> textParts3;
 
-  TextParts3({Key? key, required this.bulletPoint3, required this.textParts3}) : super(key: key);
+  TextParts3({Key? key, required this.bulletPoint3, required this.textParts3})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Text(bulletPoint3, style: const TextStyle(fontSize: 15, color: Color.fromRGBO(220, 20, 60, 1), fontWeight: FontWeight.bold)),
+        Text(bulletPoint3,
+            style: const TextStyle(
+                fontSize: 16.5,
+                color: Color.fromRGBO(220, 20, 60, 1),
+                fontWeight: FontWeight.bold)),
         // const SizedBox(width: 3),
         Expanded(
           child: RichText(
             text: TextSpan(
-              children: textParts3.map((segment) => TextSpan(text: segment.text, style: segment.style)).toList(),
+              children: textParts3
+                  .map((segment) =>
+                      TextSpan(text: segment.text, style: segment.style))
+                  .toList(),
             ),
           ),
         ),
