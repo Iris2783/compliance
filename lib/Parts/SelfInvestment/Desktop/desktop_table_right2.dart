c2079c25e184e4bdc3ca60a1d971330be507d6f4
Iris2783 <108006833+Iris2783@users.noreply.github.com>
2024-01-23 14:58:08 +0900

    Update desktop_table_right2.dart

--- lib/Parts/SelfInvestment/Desktop/desktop_table_right2.dart
+++ lib/Parts/SelfInvestment/Desktop/desktop_table_right2.dart
@@ -1,34 +1,26 @@
 // ignore_for_file: use_key_in_widget_constructors
 import 'package:flutter/material.dart';
 
-class DesktopTableRight3SI extends StatelessWidget {
+class DesktopTableRight2SI extends StatelessWidget {
   final double cellWidth = 100.0;
   final Map<int, double> rowHeights = {
-    0: 30.0,
-    1: 45.0,
-    2: 45.0,
-    3: 45.0,
-    4: 45.0,
-    5: 45.0,
+    0: 40.0,
+    1: 40.0,
   }; //行の高さを指定
 
   @override
   Widget build(BuildContext context) {
     return Table(
       columnWidths: const {
-        0: FixedColumnWidth(200.0),
-        1: FixedColumnWidth(330.0),
-        2: FixedColumnWidth(330.0),
+        0: FixedColumnWidth(120.0),
+        1: FixedColumnWidth(740.0),
       }, //列の幅を指定
       border: TableBorder.all(),
       children: [
-        buildTableRow([' ', '登録要否', '備考'],
-            isHeader: true, height: rowHeights[0]),
-        buildTableRow(['口座未保有', '必須', '保有していない旨の登録も必須'], height: rowHeights[1]),
-        buildTableRow(['野村證券口座', '必須', '-'], height: rowHeights[2]),
-        buildTableRow(['他証券口座', '必須', '保有資産の残高証明登録も必須'], height: rowHeights[3]),
-        buildTableRow(['家族口座(関与有)', '必須', '-'], height: rowHeights[4]),
-        buildTableRow(['家族口座(関与無)', '不要', '-'], height: rowHeights[5]),
+        buildTableRow(['住所', '引越しで住所が変更になった場合には速やかに変更して下さい。'],
+            height: rowHeights[0]),
+        buildTableRow(['部署名', '異動により部署が変わる場合は速やかに部署名を変更して下さい。'],
+            height: rowHeights[1]),
       ],
     );
   }
