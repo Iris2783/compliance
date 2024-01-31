b20ee9e50a2ad30e8da33ad3510ac0d616953af7
Iris2783 <iris.corp.2783@gmail.com>
2024-01-31 15:34:12 +0900

    update

--- lib/TopPage/tablet_toppage.dart
+++ lib/TopPage/tablet_toppage.dart
@@ -16,16 +16,18 @@ import 'package:flutter/material.dart';
 
 // ignore: use_key_in_widget_constructors
 class TabletTopPage extends StatelessWidget {
-  final List<String> complianceItems = [
-    '労務管理',
-    '社員投資',
-    '情報管理',
-    '受贈簿・社外交流簿',
-    '在宅勤務',
-    '安否確認システム',
+  final List<Map<String, dynamic>> complianceItems = [
+    {'title': '労務管理', 'icon': Icons.work, 'color': Colors.green},
+    {'title': '社員投資', 'icon': Icons.attach_money, 'color': Colors.green},
+    {'title': '情報管理', 'icon': Icons.info, 'color': Colors.green},
+    {'title': '受贈簿・社外交流簿', 'icon': Icons.contacts, 'color': Colors.green},
+    {'title': '在宅勤務', 'icon': Icons.home, 'color': Colors.green},
+    {'title': '安否確認システム', 'icon': Icons.check, 'color': Colors.green},
   ];
 
-  final double maxGridSize = 220.0;
+  final double maxGridSize = 200.0; // 大きさを少し大きく
+  final double containerSpacing = 25.0; // 横の間隔を開ける
+  final double verticalSpacing = 10.0; // 縦の間隔を狭める
 
   @override
   Widget build(BuildContext context) {
@@ -44,47 +46,29 @@ class TabletTopPage extends StatelessWidget {
       ),
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.only(top: 16.0),
-          child: Wrap(
-            alignment: WrapAlignment.center,
-            spacing: 16.0,
-            runSpacing: 16.0,
-            children: complianceItems.map(
-              (item) {
-                return GestureDetector(
-                  onTap: () {
-                    int index = complianceItems.indexOf(item);
-                    navigateToSubPage(context, index);
-                  },
-                  child: Container(
-                    constraints: BoxConstraints(
-                      maxWidth: maxGridSize,
-                      maxHeight: maxGridSize,
-                    ),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: const Color.fromRGBO(133, 174, 77, 1),
-                        width: 2.0,
+          padding: EdgeInsets.symmetric(vertical: verticalSpacing),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              for (var i = 0; i < complianceItems.length; i += 3)
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    for (var j = i; j < i + 3 && j < complianceItems.length; j++)
+                      ContainerWithHoverEffect(
+                        title: complianceItems[j]['title'],
+                        icon: complianceItems[j]['icon'],
+                        color: complianceItems[j]['color'],
+                        maxGridSize: maxGridSize,
+                        containerSpacing: containerSpacing,
+                        onTap: () {
+                          int index = complianceItems.indexOf(complianceItems[j]);
+                          navigateToSubPage(context, index);
+                        },
                       ),
-                      borderRadius: BorderRadius.circular(8.0),
-                      color: const Color.fromRGBO(133, 177, 77, 0.5),
-                    ),
-                    child: Center(
-                      child: Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Text(
-                          item,
-                          style: const TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ).toList(),
+                  ],
+                ),
+            ],
           ),
         ),
       ),
@@ -145,14 +129,95 @@ class TabletTopPage extends StatelessWidget {
         Navigator.push(
           context,
           MaterialPageRoute(
-                        builder: (context) => ResponsiveLayout(
-                          mobileScaffold: MobileSafetyConfirmation(),
-                          tabletScaffold: TabletSafetyConfirmation(),
-                          desktopScaffold: DesktopSafetyConfirmation(),
-                        ),
-                      ),
+            builder: (context) => ResponsiveLayout(
+              mobileScaffold: MobileSafetyConfirmation(),
+              tabletScaffold: TabletSafetyConfirmation(),
+              desktopScaffold: DesktopSafetyConfirmation(),
+            ),
+          ),
         );
         break;
+      default:
+        break;
     }
+  }
+}
+
+class ContainerWithHoverEffect extends StatefulWidget {
+  final String title;
+  final IconData icon;
+  final Color color;
+  final double maxGridSize;
+  final double containerSpacing;
+  final Function onTap;
+
+  ContainerWithHoverEffect({
+    required this.title,
+    required this.icon,
+    required this.color,
+    required this.maxGridSize,
+    required this.containerSpacing,
+    required this.onTap,
+  });
+
+  @override
+  _ContainerWithHoverEffectState createState() => _ContainerWithHoverEffectState();
+}
+
+class _ContainerWithHoverEffectState extends State<ContainerWithHoverEffect> {
+  bool isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) => _toggleHover(true),
+      onExit: (event) => _toggleHover(false),
+      child: GestureDetector(
+        onTap: () {
+          widget.onTap();
+        },
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: isHovered ? widget.maxGridSize + 20 : widget.maxGridSize,
+          height: isHovered ? widget.maxGridSize + 20 : widget.maxGridSize,
+          margin: EdgeInsets.symmetric(horizontal: widget.containerSpacing),
+          decoration: BoxDecoration(
+            border: Border.all(
+              color: isHovered ? const Color.fromRGBO(220, 20, 60, 1) : const Color.fromRGBO(133, 174, 77, 1),
+              width: 2.0,
+            ),
+            borderRadius: BorderRadius.circular(12.0),
+            color: const Color.fromRGBO(133, 177, 77, 0.5),
+          ),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  widget.icon,
+                  size: 64,
+                  color: isHovered ? const Color.fromRGBO(220, 20, 60, 1) : widget.color,
+                ),
+                const SizedBox(height: 12),
+                Text(
+                  widget.title,
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: isHovered ? const Color.fromRGBO(220, 20, 60, 1) : Colors.black,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _toggleHover(bool isHovered) {
+    setState(() {
+      this.isHovered = isHovered;
+    });
   }
 }
